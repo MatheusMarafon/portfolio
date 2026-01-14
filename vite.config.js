bee7b6a14c9617c480a2592c8cb7389b4e4b9bb6
MatheusMarafon <marafonmatheus1@gmail.com>
2026-01-14 11:02:41 -0300

    chore: config deploy to gh-pages and add education section

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/", 
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
